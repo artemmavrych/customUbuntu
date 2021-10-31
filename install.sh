c416e01ca304b9702428c9fd371680a56c1fcd68
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-10-31 16:22:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/01-preq.sh
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/configuration.txt
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/02-debootstrap.sh
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/03-chroot-settings.sh
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/04-chroot-networkClearExit.sh
