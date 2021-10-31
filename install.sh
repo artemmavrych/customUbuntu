479eeedea824a6a96698adf9e918c73c0de75bc0
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-10-31 16:20:03 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/01-preq.sh
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/configuration.txt
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/02-debootstrap.sh
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/03_denos_chroot_lxqt.sh
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/04_denos_MakeSquashfs.sh
-wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/05_denos_BiosUEFI_ISO.sh
-
-chmod 755 *.sh
