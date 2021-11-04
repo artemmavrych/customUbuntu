80d98f16a4db20b3c59491fb832db8a328c72f07
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-11-04 17:16:12 +0200

    Update 02-debootstrap.sh

--- 02-debootstrap.sh
+++ 02-debootstrap.sh
@@ -19,7 +19,7 @@ sudo cp ./xfce4-panel-menu\(22\).png $HOME/$WORKSPACE/chroot
 sudo cp ./xfce4-panel-menu\(24\).png $HOME/$WORKSPACE/chroot
 sudo cp ./xfce4-panel-menu\(32\).png $HOME/$WORKSPACE/chroot
 sudo cp ./xfce4-panel-menu\(48\).png $HOME/$WORKSPACE/chroot
-sudo cp ./theme $HOME/$WORKSPACE/chroot/
+sudo cp -r ./theme $HOME/$WORKSPACE/chroot/
 
 
 sudo mount --bind /dev $HOME/$WORKSPACE/chroot/dev
