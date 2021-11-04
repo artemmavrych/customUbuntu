7ffcebb6bf55e75dc940b5ae08c18cb8c2733786
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-11-04 16:32:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,17 @@ wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/03-chroot-
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/04-chroot-networkClearExit.sh
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/05-squashfs.sh
 wget https://raw.githubusercontent.com/artemmavrych/customUbuntu/main/06-BiosUEFI_ISO.sh
+wget https://github.com/artemmavrych/customUbuntu/raw/main/xfce4-panel-menu(16).png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/xfce4-panel-menu(22).png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/xfce4-panel-menu(24).png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/xfce4-panel-menu(32).png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/xfce4-panel-menu(48).png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/wallpaper.png
+wget https://github.com/artemmavrych/customUbuntu/raw/main/wallpaper.jpg
+wget https://github.com/artemmavrych/customUbuntu/raw/main/theme.tar.gz
+wget https://github.com/artemmavrych/customUbuntu/raw/main/200.png
 
 chmod 755 *.sh
+chmod 755 *.png
+chmod 755 *.jpg
+chmod 755 *.tar.gz
