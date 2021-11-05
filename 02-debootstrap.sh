981fdb046e3e20f767f2c3d92ddb3eefda4efa19
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-11-05 19:43:06 +0200

    Update 02-debootstrap.sh

--- 02-debootstrap.sh
+++ 02-debootstrap.sh
@@ -21,6 +21,7 @@ sudo cp ./xfce4-panel-menu\(32\).png $HOME/$WORKSPACE/chroot
 sudo cp ./xfce4-panel-menu\(48\).png $HOME/$WORKSPACE/chroot
 sudo cp -r ./theme $HOME/$WORKSPACE/chroot/
 sudo cp -r ./.config $HOME/$WORKSPACE/chroot/etc/skel/
+chmod -R 777 $HOME/$WORKSPACE/chroot/etc/skel/.config
 
 sudo mount --bind /dev $HOME/$WORKSPACE/chroot/dev
 sudo mount --bind /run $HOME/$WORKSPACE/chroot/run
