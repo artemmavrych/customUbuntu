b8ba6102cec12965af8f31dbcdb5c1d2f3222f6c
artemmavrych <80837184+artemmavrych@users.noreply.github.com>
2021-11-19 17:49:26 +0200

    Update 03-chroot-settings.sh

--- 03-chroot-settings.sh
+++ 03-chroot-settings.sh
@@ -60,7 +60,8 @@ apt-get install -y \
 
 apt-get install -y --no-install-recommends linux-generic
 #apt-get install -y grub-efi-ia32
-apt-get install -y grub-efi-amd64
+#vernut posle VirtualBox
+#apt-get install -y grub-efi-amd64
 
 #XFCE
 apt install xorg lightdm liblightdm-gobject-1-0 lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfwm4 -y
